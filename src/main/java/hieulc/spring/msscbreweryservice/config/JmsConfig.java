369cdbbb1090e4d096891b6e7474c6c8dbb93ca3
hieulc <forjob.leconghieu@gmail.com>
2023-01-10 03:22:43 +0700

    - Test Intellij setting

--- src/main/java/hieulc/spring/msscbreweryservice/config/JmsConfig.java
+++ src/main/java/hieulc/spring/msscbreweryservice/config/JmsConfig.java
@@ -2,8 +2,8 @@ package hieulc.spring.msscbreweryservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
