85c68e7c373fce38b0a83b608015e39a0348834b
hieulc <forjob.leconghieu@gmail.com>
2023-01-09 18:57:44 +0700

    - Adding @Configuration in JmsConfig.java. close #6

--- src/main/java/hieulc/spring/msscbreweryservice/config/JmsConfig.java
+++ src/main/java/hieulc/spring/msscbreweryservice/config/JmsConfig.java
@@ -1,10 +1,12 @@
 package hieulc.spring.msscbreweryservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
+@Configuration
 public class JmsConfig {
 
     @Bean
